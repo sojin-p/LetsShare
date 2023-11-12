c8ce6d7ab9340f043872cb8398342e05a1742291
Sojin Park <luv2hong@gmail.com>
2023-11-12 20:13:50 +0900

    [Test] From VC to LoginVC 이름 변경 후 화면 테스트

--- Let'sShare/Scene/LoginViewController.swift
+++ Let'sShare/Scene/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  Let'sShare
 //
 //  Created by 박소진 on 2023/11/12.
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
     }
 
 
