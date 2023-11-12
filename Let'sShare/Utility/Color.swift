7cde7d5b30da12ba2aa10285069067396e2ae601
Sojin Park <luv2hong@gmail.com>
2023-11-12 21:53:44 +0900

    [Add] Color+Enum 파일 추가

---
+++ Let'sShare/Utility/Color.swift
@@ -0,0 +1,20 @@
+//
+//  Color.swift
+//  Let'sShare
+//
+//  Created by 박소진 on 2023/11/12.
+//
+
+import UIKit
+
+enum Color {
+    
+    enum Point {
+        static let navy = UIColor(resource: .pointNavy)
+    }
+    
+    enum Background {
+        static let basic = UIColor(resource: .background)
+    }
+    
+}
