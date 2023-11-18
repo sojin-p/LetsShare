abb877c38efa0d79d94ec215fa0b1a8bd10e4b4b
Sojin Park <luv2hong@gmail.com>
2023-11-19 01:10:37 +0900

    [Add] WelcomeViewController 추가

---
+++ Let'sShare/Scene/Welcome/WelcomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  WelcomeViewController.swift
+//  Let'sShare
+//
+//  Created by 박소진 on 2023/11/19.
+//
+
+import UIKit
+
+final class WelcomeViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
