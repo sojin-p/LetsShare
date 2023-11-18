772d19f6242e8b3d89f938ecf57a6683f14d1afa
Sojin Park <luv2hong@gmail.com>
2023-11-19 02:22:26 +0900

    [Chore] SceneDelegate 첫 화면 네비게이션 바 추가

--- Let'sShare/SceneDelegate.swift
+++ Let'sShare/SceneDelegate.swift
@@ -18,9 +18,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        let vc = WelcomeViewController()
-        //UINavigationController(rootViewController: LoginViewController())
-        window?.rootViewController = vc
+        let nav = UINavigationController(rootViewController: WelcomeViewController())
+        window?.rootViewController = nav
         window?.makeKeyAndVisible()
     }
 
