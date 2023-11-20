f0247d1b9790a78e1847a3e857d0d1afea65b014
Sojin Park <luv2hong@gmail.com>
2023-11-20 22:58:12 +0900

    [Add] APIManager 파일 추가

---
+++ Let'sShare/Network/APIManager.swift
@@ -0,0 +1,17 @@
+//
+//  APIManager.swift
+//  Let'sShare
+//
+//  Created by 박소진 on 2023/11/20.
+//
+
+import Foundation
+
+final class APIManager {
+    
+    static let shared = APIManager()
+    
+    private init() { }
+    
+    
+}
