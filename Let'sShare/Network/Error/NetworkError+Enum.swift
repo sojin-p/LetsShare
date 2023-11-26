34865d9383cb99c9a3d404b18cf5e4409cce9054
Sojin Park <luv2hong@gmail.com>
2023-11-26 09:17:28 +0900

    [Style] NetworkError 열거형 코드 스타일 변경

--- Let'sShare/Network/Error/NetworkError+Enum.swift
+++ Let'sShare/Network/Error/NetworkError+Enum.swift
@@ -7,8 +7,22 @@
 
 import Foundation
 
+enum NetworkError: Error {
+    case common(_ common: CommonError)
+    case user(_ signUp: UserError)
+
+    var errorDescription: String {
+        switch self {
+        case .common(let commonError):
+            return commonError.errorDescription
+        case .user(let userError):
+            return userError.errorDescription
+        }
+    }
+}
+
 //공통 에러
-enum NetworkError: Int, Error {
+enum CommonError: Int, Error {
     case unathorized = 420
     case unavailable = 429 //과호출
     case invalidURL = 444 //비정상 url
@@ -24,6 +38,24 @@ enum NetworkError: Int, Error {
             return "유효하지 않은 주소입니다."
         case .invalidServer:
             return "서버 점검 중입니다."
+        }
+    }
+}
+
+enum UserError: Int, Error {
+    
+    case empty = 400 //필수값 누락
+    case unavailable = 409 //중복
+    case checkEmail = 401 //미가입 or 비밀번호 불일치
+    
+    var errorDescription: String {
+        switch self {
+        case .empty:
+            return "필수 항목을 입력해 주세요."
+        case .unavailable:
+            return "이미 가입된 이메일입니다."
+        case .checkEmail:
+            return "이메일 또는 비밀번호가 일치하지 않습니다."
         }
     }
 }
