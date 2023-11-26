a002dd7ec5747c5a95fdf10a70e0db3f021febb3
Sojin Park <luv2hong@gmail.com>
2023-11-26 09:22:19 +0900

    [Add] FeedVC 파일 추가

---
+++ Let'sShare/Scene/Feed/FeedViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FeedViewController.swift
+//  Let'sShare
+//
+//  Created by 박소진 on 2023/11/26.
+//
+
+import UIKit
+
+final class FeedViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .lightGray
+    }
+    
+}
