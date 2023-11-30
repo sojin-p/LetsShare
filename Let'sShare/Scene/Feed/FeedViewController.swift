1a11b809ff71a06de70e9a9b8caafbfdcb308d96
Sojin Park <luv2hong@gmail.com>
2023-11-30 20:21:52 +0900

    [Design] FeedVC Toolbar 추가

--- Let'sShare/Scene/Feed/FeedViewController.swift
+++ Let'sShare/Scene/Feed/FeedViewController.swift
@@ -17,17 +17,70 @@ final class FeedViewController: BaseViewController {
         return view
     }()
     
+    var toTopBarButton = {
+        let view = UIButton()
+        var config = UIButton.Configuration.plain()
+        config.image = UIImage(systemName: "chevron.up")
+        config.baseForegroundColor = .white
+        config.imagePadding = 5
+        view.configuration = config
+        let barButton = UIBarButtonItem(customView: view)
+        return barButton
+    }()
+    
+    var searchBarButton = {
+        let view = UIButton()
+        var config = UIButton.Configuration.plain()
+        config.title = "검색"
+        config.image = UIImage(systemName: "magnifyingglass")
+        config.baseForegroundColor = .white
+        config.imagePadding = 5
+        config.imagePlacement = .leading
+        view.configuration = config
+        let barButton = UIBarButtonItem(customView: view)
+        return barButton
+    }()
+    
+    let addBarButton = {
+        let view = UIButton()
+        var config = UIButton.Configuration.plain()
+        config.title = "글쓰기"
+        config.image = UIImage(systemName: "pencil.line")
+        config.baseForegroundColor = .white
+        config.imagePadding = 5
+        config.imagePlacement = .leading
+        view.configuration = config
+        let barButton = UIBarButtonItem(customView: view)
+        return barButton
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(#function)
         view.backgroundColor = .lightGray
         
         tableView.delegate = self
         tableView.dataSource = self
         
+        navigationController?.isToolbarHidden = false
+        setToolbarButton()
+    }
+    
+    func setToolbarButton() {
+        navigationController?.toolbar.barTintColor = Color.Point.navy
+        navigationController?.toolbar.setShadowImage(UIImage(), forToolbarPosition: .bottom)
+        var items = [UIBarButtonItem]()
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        let edgeSpace = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: self, action: nil)
+        [edgeSpace, toTopBarButton, flexibleSpace, flexibleSpace, searchBarButton, flexibleSpace, flexibleSpace, addBarButton, flexibleSpace, edgeSpace].forEach { items.append($0) }
+        self.toolbarItems = items
     }
     
     override func configure() {
-        view.addSubview(tableView)
+        [tableView].forEach { view.addSubview($0) }
+    }
+    
+    override func setConstraints() {
         tableView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
