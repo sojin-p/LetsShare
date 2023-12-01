c4e38db216021bbb132f98790ddcfcb691f3e6aa
Sojin Park <luv2hong@gmail.com>
2023-12-01 16:48:43 +0900

    [Add] APIManager provider에 interceptor 코드 추가

--- Let'sShare/Network/APIManager.swift
+++ Let'sShare/Network/APIManager.swift
@@ -14,13 +14,18 @@ final class APIManager {
     
     private init() { }
     
-    let provider = MoyaProvider<UserAPI>()
+    let provider = MoyaProvider<UserAPI>(session: Moya.Session(interceptor: AuthInterceptor.shared))
     
     func callRequest<T: Decodable, U: RawRepresentable<Int>>(type: T.Type, api: UserAPI, errorType: U.Type, completion: @escaping (Result<T, Error>) -> Void ) {
         
         provider.request(api) { result in
+            
+            print("==APIManager==", result)
+            
             switch result {
+                
             case .success(let value):
+                print("==success statusCode==", value.statusCode)
                 do {
                     let success = try value.filter(statusCode: 200)
                     let result = try JSONDecoder().decode(type, from: success.data)
@@ -29,7 +34,7 @@ final class APIManager {
                 } catch {
                     
                     let statusCode = value.statusCode
-                    
+                    print("==catch statusCode==", statusCode)
                     if let common = CommonError(rawValue: statusCode) {
                         completion(.failure(common))
                     } else if let error = errorType.init(rawValue: statusCode) as? Error {
