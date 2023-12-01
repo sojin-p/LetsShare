3a1dc30df4732763474add587b347c85bde4103e
Sojin Park <luv2hong@gmail.com>
2023-12-01 16:47:43 +0900

    [Add] API+Enum 파일 리프레시, 포스트 엔드포인트 추가

--- Let'sShare/Network/API+Enum.swift
+++ Let'sShare/Network/API+Enum.swift
@@ -12,12 +12,14 @@ enum UserAPI {
     case join(data: Join)
     case validationEmail(data: ValidationEmail)
     case login(data: Login)
+    case refresh
+    case createPost(data: Post)
 }
 
 extension UserAPI: TargetType {
     
     var baseURL: URL {
-        guard let url = URL(string: BaseURL.devURL) else {
+        guard let url = URL(string: BaseURL.authURL) else {
             fatalError("Invalid BaseURL")
         }
         return url
@@ -28,11 +30,18 @@ extension UserAPI: TargetType {
         case .join: return "join"
         case .login: return "login"
         case .validationEmail: return "validation/email"
+        case .refresh: return "refresh"
+        case .createPost: return "post"
         }
     }
     
     var method: Moya.Method {
-        return .post
+        switch self {
+        case .join, .validationEmail, .login, .createPost: 
+            return .post
+        case .refresh: 
+            return .get
+        }
     }
     
     var task: Moya.Task { //파라미터 영역
@@ -43,11 +52,37 @@ extension UserAPI: TargetType {
             return .requestJSONEncodable(data)
         case .login(let data):
             return .requestJSONEncodable(data)
+        case .refresh:
+            return .requestPlain
+        case .createPost(let data):
+            return .uploadMultipart(setMultipartData(data))
         }
     }
     
     var headers: [String : String]? {
-        ["Content-Type" : "application/json",
-         "SesacKey" : APIKey.sesac]
+        ["Content-Type": "application/json",
+         "SesacKey": APIKey.sesac]
     }
+}
+
+extension UserAPI {
+    
+    var validationType: ValidationType {
+        return .successCodes
+    }
+    
+    func setMultipartData(_ data: Post) -> [MultipartFormData] {
+        let productIDData = MultipartFormData(
+            provider: .data(data.product_id.data(using: .utf8)!),
+            name: "product_id")
+        let titleData = MultipartFormData(
+            provider: .data(data.title.data(using: .utf8)!),
+            name: "title")
+        let contentData = MultipartFormData(
+            provider: .data(data.content.data(using: .utf8)!),
+            name: "content")
+        let multipartData: [MultipartFormData] = [productIDData, titleData, contentData]
+        return multipartData
+    }
+    
 }
