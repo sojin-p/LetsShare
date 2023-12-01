f7c855391e1e0dd1bd754fcc7a3c4f9a562b6091
Sojin Park <luv2hong@gmail.com>
2023-12-01 16:43:58 +0900

    [Add] PostModel 추가

---
+++ Let'sShare/Network/Model/PostModel.swift
@@ -0,0 +1,29 @@
+//
+//  PostModel.swift
+//  Let'sShare
+//
+//  Created by 박소진 on 2023/11/30.
+//
+
+import Foundation
+
+struct Post: Encodable {
+    let title: String
+    let content: String
+    let product_id: String
+}
+
+//받는 데이터
+struct PostResponse: Decodable {
+    let _id: String
+    let creator: Creator
+    let time: String
+    let title: String
+    let content: String
+    let product_id: String
+}
+
+struct Creator: Decodable {
+    let _id: String
+    let nick: String
+}
