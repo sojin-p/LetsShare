97cbb3e9838bf2ca664cd1d6326f2b8d9c740811
Sojin Park <luv2hong@gmail.com>
2023-12-02 20:25:25 +0900

    [Design] WritingVC 디자인

--- Let'sShare/Scene/Writing/WritingViewController.swift
+++ Let'sShare/Scene/Writing/WritingViewController.swift
@@ -8,10 +8,88 @@
 import UIKit
 
 final class WritingViewController: BaseViewController {
+    
+    let selectInterestsLabel = {
+        let view = UILabel()
+        let attributedText = NSMutableAttributedString(string: "관심사 선택 ")
+        let attachment = NSTextAttachment()
+        attachment.image = UIImage(systemName: "chevron.down")
+        attachment.bounds = CGRect(x: 0, y: 0, width: 12, height: 8)
+        attributedText.append(NSAttributedString(attachment: attachment))
+        view.attributedText = attributedText
+        view.font = .systemFont(ofSize: 13, weight: .semibold)
+        view.textColor = .black
+        view.isUserInteractionEnabled = true
+        return view
+    }()
+    
+    let titleTextField = {
+        let view = UITextField()
+        view.placeholder = "제목을 입력하세요."
+        return view
+    }()
+    
+    let lineView = {
+        let view = UIView()
+        view.backgroundColor = .systemGray5
+        return view
+    }()
+    
+    let contentTextView = {
+        let view = UITextView()
+        view.text = "내용을 입력하세요."
+        view.font = .systemFont(ofSize: 16)
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = .white
+        setBarButton()
+    }
+    
+    override func configure() {
+        [selectInterestsLabel, titleTextField, lineView, contentTextView].forEach { view.addSubview($0) }
+    }
+    
+    override func setConstraints() {
+        selectInterestsLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(5)
+            make.leading.equalToSuperview().offset(20)
+            make.height.equalTo(35)
+        }
+        
+        titleTextField.snp.makeConstraints { make in
+            make.top.equalTo(selectInterestsLabel.snp.bottom)
+            make.horizontalEdges.equalToSuperview().inset(20)
+            make.height.equalTo(40)
+        }
+        
+        lineView.snp.makeConstraints { make in
+            make.top.equalTo(titleTextField.snp.bottom).offset(4)
+            make.horizontalEdges.equalTo(titleTextField)
+            make.height.equalTo(1)
+        }
+        
+        contentTextView.snp.makeConstraints { make in
+            make.top.equalTo(lineView.snp.bottom).offset(6)
+            make.horizontalEdges.equalTo(titleTextField).inset(-4)
+            make.bottom.equalTo(view.keyboardLayoutGuide.snp.top).offset(-20)
+        }
     }
 
+}
+
+extension WritingViewController {
+    
+    func setBarButton() {
+        let backBarButton = UIBarButtonItem(image: UIImage(systemName: "xmark"), style: .plain, target: self, action: #selector(backButtonClicked))
+        backBarButton.tintColor = .black
+        
+        navigationItem.leftBarButtonItem = backBarButton
+    }
+    
+    @objc func backButtonClicked() {
+        print("===뒤로 클릭")
+    }
 }
