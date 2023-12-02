b1556493302b108ce141f93d96dad60ee9a0760e
Sojin Park <luv2hong@gmail.com>
2023-12-02 16:25:00 +0900

    [Design] FeedVC 디자인 변경

--- Let'sShare/Scene/Feed/FeedViewController.swift
+++ Let'sShare/Scene/Feed/FeedViewController.swift
@@ -17,10 +17,11 @@ final class FeedViewController: BaseViewController {
         return view
     }()
     
-    var toTopBarButton = {
+    var refreshBarButton = {
         let view = UIButton()
         var config = UIButton.Configuration.plain()
-        config.image = UIImage(systemName: "chevron.up")
+        config.title = "새로고침"
+//        config.image = UIImage(systemName: "arrow.clockwise")
         config.baseForegroundColor = .white
         config.imagePadding = 5
         view.configuration = config
@@ -31,8 +32,8 @@ final class FeedViewController: BaseViewController {
     var searchBarButton = {
         let view = UIButton()
         var config = UIButton.Configuration.plain()
-        config.title = "검색"
-        config.image = UIImage(systemName: "magnifyingglass")
+        config.title = "검색하기"
+//        config.image = UIImage(systemName: "magnifyingglass")
         config.baseForegroundColor = .white
         config.imagePadding = 5
         config.imagePlacement = .leading
@@ -41,15 +42,16 @@ final class FeedViewController: BaseViewController {
         return barButton
     }()
     
-    let addBarButton = {
-        let view = UIButton()
+    lazy var addBarButton = {
+        let view = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.width / 3, height: 0))
         var config = UIButton.Configuration.plain()
         config.title = "글쓰기"
-        config.image = UIImage(systemName: "pencil.line")
+//        config.image = UIImage(systemName: "pencil.line")
         config.baseForegroundColor = .white
         config.imagePadding = 5
         config.imagePlacement = .leading
         view.configuration = config
+        view.addTarget(self, action: #selector(addBarButtonClicked), for: .touchUpInside)
         let barButton = UIBarButtonItem(customView: view)
         return barButton
     }()
@@ -62,18 +64,28 @@ final class FeedViewController: BaseViewController {
         tableView.delegate = self
         tableView.dataSource = self
         
-        navigationController?.isToolbarHidden = false
         setToolbarButton()
     }
     
-    func setToolbarButton() {
-        navigationController?.toolbar.barTintColor = Color.Point.navy
-        navigationController?.toolbar.setShadowImage(UIImage(), forToolbarPosition: .bottom)
-        var items = [UIBarButtonItem]()
-        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
-        let edgeSpace = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: self, action: nil)
-        [edgeSpace, toTopBarButton, flexibleSpace, flexibleSpace, searchBarButton, flexibleSpace, flexibleSpace, addBarButton, flexibleSpace, edgeSpace].forEach { items.append($0) }
-        self.toolbarItems = items
+    @objc func addBarButtonClicked() {
+        print("====clicked")
+        
+        let data = Post(title: "제목입니다", content: "본문입니다~", product_id: "letsShare_sojin_id")
+        APIManager.shared.callRequest(type: PostResponse.self, api: .createPost(data: data), errorType: AccessTokenError.self) { response in
+            switch response {
+            case .success(let success):
+                print("==== 메세지: ", success)
+            case .failure(let failure):
+                if let common = failure as? CommonError {
+                    print("=== CommonError: ", common.errorDescription)
+                } else if let error = failure as? AccessTokenError {
+                    print("=== AccessTokenError: ", error.errorDescription)
+                    if error.rawValue == 418 {
+                        print("== 다시 로그인~!")
+                    }
+                }
+            }
+        }
     }
     
     override func configure() {
@@ -103,5 +115,16 @@ extension FeedViewController: UITableViewDelegate, UITableViewDataSource {
 }
 
 extension FeedViewController {
+    
+    func setToolbarButton() {
+        navigationController?.isToolbarHidden = false
+        navigationController?.toolbar.barTintColor = Color.Point.navy
+        navigationController?.toolbar.setShadowImage(UIImage(), forToolbarPosition: .bottom)
+        var items = [UIBarButtonItem]()
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        let edgeSpace = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: self, action: nil)
+        [edgeSpace, refreshBarButton, flexibleSpace, addBarButton, flexibleSpace, searchBarButton, edgeSpace].forEach { items.append($0) }
+        self.toolbarItems = items
+    }
     
 }
