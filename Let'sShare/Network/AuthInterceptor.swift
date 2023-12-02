cd9049215c0fe82cb1b09a7b732e9b97d739d56d
Sojin Park <luv2hong@gmail.com>
2023-12-02 16:24:27 +0900

    [Fix] AuthInterceptor 토큰 갱신 안되는 이슈 해결
    - adapt에 refresh 토큰 세팅

--- Let'sShare/Network/AuthInterceptor.swift
+++ Let'sShare/Network/AuthInterceptor.swift
@@ -16,12 +16,17 @@ final class AuthInterceptor: RequestInterceptor {
     
     func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
         
+        print(#function, "== adapt 헤더 끼워넣기!!")
+        
         var urlRequest = urlRequest
         let accessToken = UserDefaultsManager.access.myValue
-        urlRequest.headers.add(.authorization(accessToken))
+        let refreshToken = UserDefaultsManager.refresh.myValue
         
-        print("===== urlRequest ====", urlRequest, urlRequest.headers)
+        urlRequest.setValue(accessToken, forHTTPHeaderField: "Authorization")
+        urlRequest.setValue(refreshToken, forHTTPHeaderField: "Refresh")
+        
         completion(.success(urlRequest))
+        
     }
     
     func retry(_ request: Request, for session: Session, dueTo error: Error, completion: @escaping (RetryResult) -> Void) {
@@ -29,26 +34,23 @@ final class AuthInterceptor: RequestInterceptor {
         print(#function)
         
         guard let response = request.task?.response as? HTTPURLResponse, response.statusCode == 419 else {
+            print("===리트라이 가드문 419가 아닐때")
             completion(.doNotRetryWithError(error))
             return
         }
         
-        print(#function)
+        print("====리트라이 액세스 토큰 갱신 필요!!")
         
         DispatchQueue.main.async {
             APIManager.shared.callRequest(type: AccessTokenResponse.self, api: .refresh, errorType: AccessTokenError.self) { response in
                 switch response {
                 case .success(let success):
                     print("== 재발급 성공 : ", success)
+                    UserDefaultsManager.access.myValue = success.token
                     completion(.retry)
                 case .failure(_):
-                    print("== 실패")
+                    print("== 재발급 실패, 재로그인 필요")
                     completion(.doNotRetryWithError(error))
-    //                if let common = failure as? CommonError {
-    //                    print("=== retry 에러: ", common.errorDescription)
-    //                } else if let error = failure as? AccessTokenError {
-    //                    print("=== retry 에러: ", error.errorDescription)
-    //                }
                 }
             }
         }
