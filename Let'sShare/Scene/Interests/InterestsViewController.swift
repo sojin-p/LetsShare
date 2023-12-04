bc997044a908bfc565edd27dc8dcf5091bb574c5
Sojin Park <luv2hong@gmail.com>
2023-12-04 19:37:52 +0900

    [Add] InterestsVC 파일 추가

---
+++ Let'sShare/Scene/Interests/InterestsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  InterestsViewController.swift
+//  Let'sShare
+//
+//  Created by 박소진 on 2023/12/04.
+//
+
+import UIKit
+
+class InterestsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
