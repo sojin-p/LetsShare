8b23cc88d3b7df934ecc8253b104167790a0273c
Sojin Park <luv2hong@gmail.com>
2024-01-16 09:55:48 +0900

    [Add] PostDetailVC 추가

---
+++ Let'sShare/Scene/PostDetail/PostDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PostDetailViewController.swift
+//  Let'sShare
+//
+//  Created by 박소진 on 2024/01/15.
+//
+
+import UIKit
+
+final class PostDetailViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .pointYellow
+        title = "상세화면"
+    }
+    
+}
