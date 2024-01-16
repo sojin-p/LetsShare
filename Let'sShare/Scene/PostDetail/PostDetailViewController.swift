b77b62a6238ec28cdb79569be5f31f2dd2f2161f
Sojin Park <luv2hong@gmail.com>
2024-01-16 14:56:29 +0900

    [Add] PostDetailVC 테이블 뷰 추가 및 테스트

--- Let'sShare/Scene/PostDetail/PostDetailViewController.swift
+++ Let'sShare/Scene/PostDetail/PostDetailViewController.swift
@@ -8,11 +8,49 @@
 import UIKit
 
 final class PostDetailViewController: BaseViewController {
+    
+    let tableView = {
+        let view = UITableView()
+        view.register(PostDetailTableViewCell.self, forCellReuseIdentifier: PostDetailTableViewCell.identifier)
+        view.rowHeight = 200
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .pointYellow
         title = "상세화면"
+        tableView.dataSource = self
+    }
+    
+    override func configure() {
+        [tableView].forEach { view.addSubview($0) }
+    }
+    
+    override func setConstraints() {
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+}
+
+extension PostDetailViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: PostDetailTableViewCell.identifier) as? PostDetailTableViewCell else { return UITableViewCell() }
+        
+        return cell
+        
     }
     
 }
