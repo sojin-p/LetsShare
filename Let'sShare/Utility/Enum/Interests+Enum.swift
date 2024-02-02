ff199df941fef8e63437c4c7d9be87d5a8f52f78
Sojin Park <luv2hong@gmail.com>
2024-02-02 15:37:07 +0900

    [Add] 관심사 종류 추가

--- Let'sShare/Utility/Enum/Interests+Enum.swift
+++ Let'sShare/Utility/Enum/Interests+Enum.swift
@@ -10,12 +10,12 @@ import Foundation
 enum Interests: String, CaseIterable {
     
     case all = "ALL"
+    case free = "자유게시판"
     case food = "맛집/요리"
     case trip = "여행"
     case exercise = "운동/스포츠"
     case culture = "문화/예술"
-    case it = "IT"
-    case education = "교육"
+    case education = "IT/교육"
     case society = "사회/정치"
     case movie = "영화/드라마"
     case nature = "자연/환경"
@@ -23,7 +23,7 @@ enum Interests: String, CaseIterable {
     var productID: String {
         switch self {
         case .all: "letsShare_sojin_id"
-        case .food, .trip, .exercise, .culture, .it, .education, .society, .movie, .nature: "letsShare_sojin_\(self)"
+        case .free, .food, .trip, .exercise, .culture, .education, .society, .movie, .nature: "letsShare_sojin_\(self)"
         }
     }
     
